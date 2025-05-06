22ae676dfe8b6b55d5489816c290cd45b42a5f69
f4-bit <fabiancquinterop@gmail.com>
2025-05-05 22:47:59 -0500

    Validators implementation and its effect on database.py

--- db/database.py
+++ db/database.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from utils.validators import initialize_collections
 import os
 
 load_dotenv()
 
-uri = f"mongodb+srv://nutritionai:{os.getenv('db_password')}@as-ai-nutrition.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+uri = os.getenv('MONGO_URI')
 client = MongoClient(uri)
+
+db = client["Nutrikids"]
+
+initialize_collections(db)
